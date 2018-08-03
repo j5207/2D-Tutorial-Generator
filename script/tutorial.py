1f5a1b20bec3f47163d61221660e2f0f87c73a8f
jhan53 <jhan53@jhu.edu>
2018-08-03 00:03:34 -0400

    update

--- script/tutorial.py
+++ script/tutorial.py
@@ -6,8 +6,14 @@ import math
 
 
 
-def cartoon(input_image, a, N, p):
+def cartoon(input_image, a=14, N=3, p=43):
     input_image = cv2.imread(input_image)
+    hsv = cv2.cvtColor(input_image,cv2.COLOR_BGR2HSV)
+    green_mask = cv2.inRange(hsv, np.array([63,101,61]), np.array([86,255,255]))
+    green_mask = cv2.dilate(green_mask, kernel = np.ones((3,3),np.uint8))
+    #green_mask = 255 - green_mask
+    res = cv2.bitwise_and(input_image, input_image, mask=green_mask)
+    input_image = cv2.subtract(input_image, res)
     for _ in range(0,N):
         bilateral_filtimg = cv2.bilateralFilter(input_image,9,75,75)
 
@@ -43,15 +49,54 @@ def cartoon(input_image, a, N, p):
                 finalimg.itemset((i,j,0),0)
                 finalimg.itemset((i,j,1),0)
                 finalimg.itemset((i,j,2),0)
-    
+
+    return finalimg
     cv2.imshow('Toonified Image',finalimg)       
     cv2.waitKey(0)  
+
+def padding(img, height, width):
+    if img.shape[0] < height or img.shape[1] < width:
+        offset = (((height-img.shape[0])//2, height-img.shape[0]-
+        (height-img.shape[0])//2), ((width-img.shape[1])//2, width-img.shape[1]-
+        (width-img.shape[1])//2), (0, 0))
+        resize = np.pad(img, offset, 'constant')
+        resize = add_background(resize)
+        if resize.shape == (height, width):
+            raise Exception('sorry i am fool')
+        return resize
+    else:
+        return img
+
+
+
+def concat_imgs(images):
+    image_list = list(map(cartoon, images))
+    height, width = 300, 200
+    for i, image in enumerate(image_list):
+        if i == 0:
+            img1 = image
+        else:
+            img1 = np.concatenate((padding(img1, *(height, width)), padding(image, *(height, width))), axis=1)
+    return img1
+
+
+def add_background(image):
+    background = cv2.imread('background.jpg')
+    background = background[:image.shape[0], :image.shape[1]]
+    image[image == 0] = background[image == 0]
+    return image
+
 
 
 def main():
     file = open("datasets/node.txt", "r")
     for lines in file:
-        print(lines[0])
-    cartoon('2.jpg',14, 3, 43)
+        id1, id2, side1, side2 = int(lines[1]), int(lines[4]), str(lines[8:11]), str(lines[15:19])
+        print(id1, id2, side1, side2)
+    canvas = concat_imgs(['1.jpg', '2.jpg', '5.jpg'])
+    canvas[canvas == 0] = 255
+    cv2.imshow('ddd', canvas)
+    cv2.waitKey(0)
 
-main()
+if __name__ == '__main__':
+    main()
